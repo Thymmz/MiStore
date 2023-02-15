e66c44534d272c2473f1b43ab109d12a8f22293d
Thymmz <atharva.deshpande228@gmail.com>
2023-02-15 11:35:26 -0600

    offers commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import './App.css';
 import PreNavbar from './components/PreNavbar';
 import Navbar from './components/Navbar';
 import Slider from './components/Slider.js';
+import Offers from './components/Offers.js';
+
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const banner = require('./data/data.json').banner
+const offer = require('./data/data.json').offer;
+
+
 function App() {
   return (
     <Router>
       <PreNavbar />
       <Navbar />
       <Slider start={banner.start}/>
+      <Offers offer={offer}/>
     </Router>
   );
 }
