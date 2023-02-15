0b05d02746d24118143f07ff7c58ab75cf3a5c87
Thymmz <atharva.deshpande228@gmail.com>
2023-02-15 10:40:59 -0600

    carousel commit

---
+++ src/components/Slider.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Carousel } from 'react-bootstrap';
+import { CarouselItem } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Slider = ({start}) => {
+  return (
+    <Carousel fade>
+        {start.map((item, index)=>(
+            <CarouselItem>
+                <img
+                className="d-block w-100"
+                src={item}
+                alt="First slide"
+                />
+            </CarouselItem>
+        ))}
+    </Carousel>
+  )
+}
+
+export default Slider
