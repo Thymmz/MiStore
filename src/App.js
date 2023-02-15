ef23affd8522ed130a432a9c96f2bcdc707396de
Thymmz <atharva.deshpande228@gmail.com>
2023-02-15 10:41:28 -0600

    carousel commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import PreNavbar from './components/PreNavbar';
 import Navbar from './components/Navbar';
+import Slider from './components/Slider.js';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+const banner = require('./data/data.json').banner
 function App() {
   return (
     <Router>
       <PreNavbar />
       <Navbar />
+      <Slider start={banner.start}/>
     </Router>
   );
 }
