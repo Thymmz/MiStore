3474cd0725c479515ce11da7bfe2543e55768651
Thymmz <atharva.deshpande228@gmail.com>
2023-02-16 10:16:11 -0600

    hotAccessories commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import Offers from './components/Offers.js';
 import Heading from './components/Heading.js';
 import StarProduct from './components/StarProduct.js'
 import HotAccessoriesMenu from './components/HotAccessoriesMenu.js'
+import HotAccessories from './components/HotAccessories.js'
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const banner = require('./data/data.json').banner
 const offer = require('./data/data.json').offer;
 const starProduct = require('./data/data.json').starProduct;
+const hotAccessories = require('./data/data.json').hotAccessories;
+const hotAccessoriesCover = require('./data/data.json').hotAccessoriesCover;
 
 function App() {
   return (
@@ -24,6 +27,16 @@ function App() {
       <StarProduct starProduct={starProduct}/>
       <Heading text="HOT ACCESSORIES"/>
       <HotAccessoriesMenu />
+      
+      <Routes>
+      <Route exact path="/music"  element = {<HotAccessories music={hotAccessories.music} musicCover={hotAccessoriesCover.music}/>}/>
+      <Route exact path="/smartDevice"  element = {<HotAccessories smartDevice={hotAccessories.smartDevice} smartDeviceCover={hotAccessoriesCover.smartDevice}/>}/>
+      <Route exact path="/home"  element = {<HotAccessories home={hotAccessories.home} homeCover={hotAccessoriesCover.home}/>}/>
+      <Route exact path="/lifestyle"  element = {<HotAccessories lifestyle={hotAccessories.lifeStyle} lifestyleCover={hotAccessoriesCover.lifeStyle}/>}/>
+      <Route exact path="/mobileAccessories"  element = {<HotAccessories mobileAccessories={hotAccessories.mobileAccessories} mobileAccessoriesCover={hotAccessoriesCover.mobileAccessories}/>}/>
+      </Routes>
+
+
     </Router>
   );
 }
